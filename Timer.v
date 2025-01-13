db02209421ba369075f79a9576c032442ef5f25c
黃俊誠 <s111060003@m111.nthu.edu.tw>
2025-01-14 03:40:26 +0800

    Update Timer.v

--- Timer.v
+++ Timer.v
@@ -1,23 +1,5 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/08/2025 05:13:44 AM
-// Design Name: 
-// Module Name: Timer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+`define IDLE     0
+`define RUNNING  1
 
 module Timer (
   input  wire        clk,
@@ -28,53 +10,44 @@ module Timer (
   output reg         curr_end_q
 );
 
-  reg        curr_state_q;
-  reg        next_state_d;
-  reg [15:0] next_time_d;
-  reg        next_end_d;
-  
+  reg curr_state_q;
+
   always @(posedge clk or negedge rst_n) begin
     if (~rst_n) begin
-      curr_state_q <= 0;
+      curr_state_q <= `IDLE;
       curr_time_q  <= 0;
       curr_end_q   <= 0;
     end else begin
-      curr_state_q <= next_state_d;
-      curr_time_q  <= next_time_d;
-      curr_end_q   <= next_end_d;
+      case (curr_state_q)
+        `IDLE: begin
+          if (start_i == 1'd1) begin
+            curr_state_q <= `RUNNING;
+            curr_time_q  <= 0;
+            curr_end_q   <= 0;
+          end else begin
+            curr_state_q <= `IDLE;
+            curr_time_q  <= 0;
+            curr_end_q   <= 0;
+          end
+        end
+        `RUNNING: begin
+          if (curr_time_q == (n_i - 1)) begin
+            curr_state_q <= `IDLE;
+            curr_time_q  <= 0;
+            curr_end_q   <= 1;
+          end else begin
+            curr_state_q <= `RUNNING;
+            curr_time_q  <= curr_time_q + 1;
+            curr_end_q   <= 0;
+          end
+        end
+        default: begin
+          curr_state_q <= `IDLE;
+          curr_time_q  <= 0;
+          curr_end_q   <= 0;
+        end
+      endcase
     end
-  end
-  
-  always @* begin
-    case (curr_state_q)
-    0: begin
-      if (start_i == 1'd1) begin
-        next_state_d = 1;
-        next_time_d  = 0;
-        next_end_d   = 0;
-      end else begin
-        next_state_d = 0;
-        next_time_d  = 0;
-        next_end_d   = 0;
-      end
-    end
-    1: begin
-      if (curr_time_q == (n_i - 1)) begin
-        next_state_d = 0;
-        next_time_d  = 0;
-        next_end_d   = 1;
-      end else begin
-        next_state_d = 1;
-        next_time_d  = curr_time_q + 1;
-        next_end_d   = 0;
-      end
-    end
-    default: begin
-      next_state_d = 0;
-      next_time_d  = 0;
-      next_end_d   = 0;
-    end
-    endcase
   end
 
 endmodule
