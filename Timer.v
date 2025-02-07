144eb36aa37b60014fb693a8b9017f0b453013ed
黃俊誠 <s111060003@m111.nthu.edu.tw>
2025-02-08 03:00:14 +0800

    Update Timer.v

--- Timer.v
+++ Timer.v
@@ -1,60 +1,62 @@
-`define IDLE     0
-`define RUNNING  1
-
-module Timer (
+module Timer #(
+  parameter IDLE    = 1'b0,
+  parameter RUNNING = 1'b1
+) (
   input  wire        clk,
   input  wire        rst_n,
   input  wire [15:0] n_i,
   input  wire        start_i,
-  output reg [15:0]  curr_time_q,
+  output reg  [15:0] curr_time_q,
   output reg         curr_end_q
 );
 
+  //////////////////////////////////
+  // State and Internal Registers //
+  //////////////////////////////////
   reg curr_state_q, next_state_d;
   reg [15:0] next_time_d;
   reg        next_end_d;
 
-  // Combinational logic for next-state and outputs
+  /////////////////////////
+  // Combinational Logic //
+  /////////////////////////
   always @(*) begin
     case (curr_state_q)
-      `IDLE: begin
-        if (start_i == 1'd1) begin
-          next_state_d = `RUNNING;
-          next_time_d  = 0;
-          next_end_d   = 0;
-        end else begin
-          next_state_d = `IDLE;
-          next_time_d  = 0;
-          next_end_d   = 0;
+      IDLE: begin
+        next_time_d = 16'd0;
+        next_end_d  = 1'b0;
+        if (start_i) begin
+          next_state_d = RUNNING;
         end
       end
 
-      `RUNNING: begin
+      RUNNING: begin
         if (curr_time_q == (n_i - 1)) begin
-          next_state_d = `IDLE;
-          next_time_d  = 0;
-          next_end_d   = 1;
+          next_state_d = IDLE;
+          next_time_d  = 16'd0;
+          next_end_d   = 1'b1;
         end else begin
-          next_state_d = `RUNNING;
-          next_time_d  = curr_time_q + 1;
-          next_end_d   = 0;
+          next_time_d = curr_time_q + 1;
+          next_end_d  = 1'b0;
         end
       end
 
       default: begin
-        next_state_d = `IDLE;
-        next_time_d  = 0;
-        next_end_d   = 0;
+        next_state_d = IDLE;
+        next_time_d  = 16'd0;
+        next_end_d   = 1'b0;
       end
     endcase
   end
 
-  // Sequential logic for state and output registers
+  //////////////////////
+  // Sequential Logic //
+  //////////////////////
   always @(posedge clk or negedge rst_n) begin
-    if (~rst_n) begin
-      curr_state_q <= `IDLE;
-      curr_time_q  <= 0;
-      curr_end_q   <= 0;
+    if (!rst_n) begin
+      curr_state_q <= IDLE;
+      curr_time_q  <= 16'd0;
+      curr_end_q   <= 1'b0;
     end else begin
       curr_state_q <= next_state_d;
       curr_time_q  <= next_time_d;
